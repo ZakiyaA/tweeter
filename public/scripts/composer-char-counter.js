4bd3b9f5fb87777e886a71cd68f1bccfc7f29320
ZakiyaAlfughi <zakiyaalfughi@gmail.com>
2021-12-15 04:39:04 +0000

    Fix display color of the counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,1 +1,22 @@
-console.log("composer-chat-counter.js");
+
+ 
+$(document).ready(function () {
+  console.log("hellllo");
+});
+
+$(document).ready(function () {
+  var maxLength = 140;
+  $('#textarea_feedback').text(maxLength);
+
+  $('#textarea').keyup(function () {
+    var length = $(this).val().length;
+    var text_remaining = maxLength - length;
+    $('#textarea_feedback').text(text_remaining);
+    if (text_remaining < 0) {
+      $("#textarea_feedback").css("color", "red");
+    } else {
+      $("#textarea_feedback").css("color", "black");
+    }
+
+  });
+});
